0a5547ab3d902babc92fa2a77c3b55dae4084337
Federico Farias Sánchez <federico.farias@optimissa.com.mx>
2017-04-14 17:11:22 -0500

    Se integró REST para búsqueda de depositantes según afianzado

---
+++ Com.PGJ.SistemaPolizas/Com.PGJ.SistemaPolizas.Service/AfianzadosService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.PGJ.SistemaPolizas.Service.Dto;
+using Com.PGJ.SistemaPolizas.Data.Model;
+
+namespace Com.PGJ.SistemaPolizas.Service
+{
+    public class AfianzadosService
+    {
+        public Task<List<DepositanteDto>> FindDepositantesFilterAsync(int afianzadoId, string filter, string sorting = "asc")
+        {
+            return Task.FromResult(FindDepositantesFilter(afianzadoId, filter, sorting));
+        }
+
+        private List<DepositanteDto> FindDepositantesFilter(int afianzadoId, string filter, string sorting = "asc")
+        {
+            using (PGJSistemaPolizasEntities db = new PGJSistemaPolizasEntities())
+            {
+                var afianzado = db.Afianzados.Where(a => a.Id == afianzadoId).FirstOrDefault();
+                var depositantes = afianzado.Depositantes.Select(e => new DepositanteDto
+                {
+                    Id = e.Id,
+                    Nombre = e.Nombre,
+                    ApellidoMaterno = e.ApellidoMaterno,
+                    ApellidoPaterno = e.ApellidoPaterno,
+                    AfianzadoId = e.AfianzadoId
+                });
+
+                depositantes = depositantes.Where(e => e.Nombre.Contains("") || e.ApellidoMaterno.Contains("") || e.ApellidoPaterno.Contains(""));
+                if (filter != null)
+                {
+                    depositantes = depositantes.Where(e => (e.ApellidoMaterno + e.ApellidoPaterno + e.Nombre).Contains(filter));
+                }
+
+                if (sorting == "asc")
+                {
+                    depositantes = depositantes.OrderBy(e => e.Nombre);
+                }
+                else
+                {
+                    depositantes = depositantes.OrderByDescending(e => e.Nombre);
+                }
+
+                return depositantes.ToList();
+            }
+        }
+    }
+}
---
+++ Com.PGJ.SistemaPolizas/Com.PGJ.SistemaPolizas/Controllers/Api/AfianzadosController.cs
@@ -0,0 +1,44 @@
+﻿using Com.PGJ.SistemaPolizas.Models;
+using Com.PGJ.SistemaPolizas.Service;
+using Com.PGJ.SistemaPolizas.Service.Dto;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Com.PGJ.SistemaPolizas.Controllers.Api
+{
+    // [Authorize]
+    [RoutePrefix("api/afianzados")]
+    public class AfianzadosController : ApiController
+    {
+        private AfianzadosService service;
+
+        public AfianzadosController()
+        {
+            service = new AfianzadosService();
+        }
+
+        [Route("{afianzadoId}/test")]
+        [HttpGet]
+        public int get(int afianzadoId)
+        {
+            return afianzadoId;
+        }
+
+        [Route("{afianzadoId}/depositantes")]
+        [HttpGet]
+        public async Task<SearchResultViewModel> Search(int afianzadoId, int page = 1, int count = 10, string sorting = "asc", string filter = "")
+        {
+            SearchResultViewModel response = new SearchResultViewModel();
+            List<DepositanteDto> list = await service.FindDepositantesFilterAsync(afianzadoId, filter, sorting);
+            response.total = list.Count();
+            response.result = list.ToPagedList(page, count);
+            return response;
+        }
+    }
+}
