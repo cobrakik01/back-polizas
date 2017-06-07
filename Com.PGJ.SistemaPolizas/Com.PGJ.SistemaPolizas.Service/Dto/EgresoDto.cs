224acb1d8ca45b6571e2793dc252537c0427e535
Federico Farias Sánchez <federico.farias@optimissa.com.mx>
2017-06-07 15:42:41 -0500

    Se integró Map y UnMap de Egresos

--- Com.PGJ.SistemaPolizas/Com.PGJ.SistemaPolizas.Service/Dto/EgresoDto.cs
+++ Com.PGJ.SistemaPolizas/Com.PGJ.SistemaPolizas.Service/Dto/EgresoDto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Com.PGJ.SistemaPolizas.Data.Model;
+using Nelibur.ObjectMapper;
 
 namespace Com.PGJ.SistemaPolizas.Service.Dto
 {
@@ -17,5 +19,28 @@ namespace Com.PGJ.SistemaPolizas.Service.Dto
 
         public AutoridadDto Autoridads { get; set; }
         public DetalleUsuarioDto DetallesUsuarios { get; set; }
+        public List<MinisteriosPublicos> MinisteriosPublicos { get; set; }
+
+        internal static List<EgresoDto> ToMap(List<Egresos> entityList)
+        {
+            List<EgresoDto> dtoList = (from entity in entityList select ToMap(entity)).ToList();
+            return dtoList;
+        }
+
+        internal static EgresoDto ToMap(Egresos entity)
+        {
+            if (entity == null) return null;
+            TinyMapper.Bind<Egresos, EgresoDto>();
+            EgresoDto dto = TinyMapper.Map<EgresoDto>(entity);
+            return dto;
+        }
+
+        internal static Egresos ToUnMap(EgresoDto dto)
+        {
+            if (dto == null) return null;
+            TinyMapper.Bind<EgresoDto, Egresos>();
+            Egresos entity = TinyMapper.Map<Egresos>(dto);
+            return entity;
+        }
     }
 }
